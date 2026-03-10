a7f912f5d98195b3212b7154905c158a1ae0ae62
Binaek Sarkar <binaek89@gmail.com>
2026-03-10 11:56:41 +0530

    Align method order with RegExp interface

--- concurrent.go
+++ concurrent.go
@@ -14,6 +14,33 @@ type concurrentRegExpImpl struct {
 
 // --- ConcurrentRegExp: thread-safe wrapper with double-checked locking ---
 
+func (c *concurrentRegExpImpl) Match(b []byte) bool {
+	c.mu.RLock()
+	state := 0
+	cacheMiss := false
+	for pos := 0; pos < len(b); {
+		r, size := utf8.DecodeRune(b[pos:])
+		mintermID := c.re.minterms.runeToClass(r)
+		next, ok := c.re.forward.getNextStateCached(state, mintermID)
+		if !ok {
+			cacheMiss = true
+			break
+		}
+		state = next
+		pos += size
+	}
+	if cacheMiss {
+		c.mu.RUnlock()
+		c.mu.Lock()
+		result := c.re.Match(b)
+		c.mu.Unlock()
+		return result
+	}
+	result := c.re.forward.isAccepting(state)
+	c.mu.RUnlock()
+	return result
+}
+
 func (c *concurrentRegExpImpl) MatchString(s string) bool {
 	c.mu.RLock()
 	state := 0
@@ -41,31 +68,8 @@ func (c *concurrentRegExpImpl) MatchString(s string) bool {
 	return result
 }
 
-func (c *concurrentRegExpImpl) Match(b []byte) bool {
-	c.mu.RLock()
-	state := 0
-	cacheMiss := false
-	for pos := 0; pos < len(b); {
-		r, size := utf8.DecodeRune(b[pos:])
-		mintermID := c.re.minterms.runeToClass(r)
-		next, ok := c.re.forward.getNextStateCached(state, mintermID)
-		if !ok {
-			cacheMiss = true
-			break
-		}
-		state = next
-		pos += size
-	}
-	if cacheMiss {
-		c.mu.RUnlock()
-		c.mu.Lock()
-		result := c.re.Match(b)
-		c.mu.Unlock()
-		return result
-	}
-	result := c.re.forward.isAccepting(state)
-	c.mu.RUnlock()
-	return result
+func (c *concurrentRegExpImpl) FindIndex(b []byte) []int {
+	return c.FindStringIndex(string(b))
 }
 
 func (c *concurrentRegExpImpl) FindStringIndex(s string) []int {
@@ -182,14 +186,6 @@ func (c *concurrentRegExpImpl) findStringIndexCached(s string) ([]int, bool) {
 	return []int{leftmostStart, longestEnd}, false
 }
 
-func (c *concurrentRegExpImpl) FindString(s string) string {
-	loc := c.FindStringIndex(s)
-	if loc == nil {
-		return ""
-	}
-	return s[loc[0]:loc[1]]
-}
-
 func (c *concurrentRegExpImpl) Find(b []byte) []byte {
 	loc := c.FindStringIndex(string(b))
 	if loc == nil {
@@ -198,8 +194,25 @@ func (c *concurrentRegExpImpl) Find(b []byte) []byte {
 	return b[loc[0]:loc[1]]
 }
 
-func (c *concurrentRegExpImpl) FindIndex(b []byte) []int {
-	return c.FindStringIndex(string(b))
+func (c *concurrentRegExpImpl) FindString(s string) string {
+	loc := c.FindStringIndex(s)
+	if loc == nil {
+		return ""
+	}
+	return s[loc[0]:loc[1]]
+}
+
+func (c *concurrentRegExpImpl) FindAll(b []byte, n int) [][]byte {
+	s := string(b)
+	locs := c.FindAllStringIndex(s, n)
+	if len(locs) == 0 {
+		return nil
+	}
+	out := make([][]byte, len(locs))
+	for i, loc := range locs {
+		out[i] = b[loc[0]:loc[1]]
+	}
+	return out
 }
 
 func (c *concurrentRegExpImpl) FindAllString(s string, n int) []string {
@@ -222,6 +235,10 @@ func (c *concurrentRegExpImpl) FindAllString(s string, n int) []string {
 	return matches
 }
 
+func (c *concurrentRegExpImpl) FindAllIndex(b []byte, n int) [][]int {
+	return c.FindAllStringIndex(string(b), n)
+}
+
 func (c *concurrentRegExpImpl) FindAllStringIndex(s string, n int) [][]int {
 	var matches [][]int
 	pos := 0
@@ -242,77 +259,29 @@ func (c *concurrentRegExpImpl) FindAllStringIndex(s string, n int) [][]int {
 	return matches
 }
 
-func (c *concurrentRegExpImpl) FindAll(b []byte, n int) [][]byte {
-	s := string(b)
-	locs := c.FindAllStringIndex(s, n)
-	if len(locs) == 0 {
+func (c *concurrentRegExpImpl) FindSubmatch(b []byte) [][]byte {
+	c.mu.RLock()
+	loc, cacheMiss := c.findStringIndexCached(string(b))
+	if cacheMiss {
+		c.mu.RUnlock()
+		c.mu.Lock()
+		out := c.re.FindSubmatch(b)
+		c.mu.Unlock()
+		return out
+	}
+	if loc == nil {
+		c.mu.RUnlock()
 		return nil
 	}
-	out := make([][]byte, len(locs))
-	for i, loc := range locs {
-		out[i] = b[loc[0]:loc[1]]
+	if c.re.CaptureCount == 0 {
+		c.mu.RUnlock()
+		return [][]byte{b[loc[0]:loc[1]]}
 	}
+	c.mu.RUnlock()
+	c.mu.Lock()
+	out := c.re.FindSubmatch(b)
+	c.mu.Unlock()
 	return out
-}
-
-func (c *concurrentRegExpImpl) FindAllIndex(b []byte, n int) [][]int {
-	return c.FindAllStringIndex(string(b), n)
-}
-
-func (c *concurrentRegExpImpl) Split(s string, n int) []string {
-	if n == 0 {
-		return nil
-	}
-	if n == 1 {
-		return []string{s}
-	}
-	var result []string
-	pos := 0
-	splits := 0
-	maxSplits := n - 1
-	for pos <= len(s) {
-		loc := c.FindStringIndex(s[pos:])
-		if loc == nil {
-			result = append(result, s[pos:])
-			break
-		}
-		start := pos + loc[0]
-		end := pos + loc[1]
-		result = append(result, s[pos:start])
-		splits++
-		if n > 0 && splits >= maxSplits {
-			result = append(result, s[end:])
-			break
-		}
-		if end == start {
-			pos++
-		} else {
-			pos = end
-		}
-	}
-	return result
-}
-
-func (c *concurrentRegExpImpl) ReplaceAllString(s, repl string) string {
-	var buf []byte
-	pos := 0
-	for pos <= len(s) {
-		loc := c.FindStringIndex(s[pos:])
-		if loc == nil {
-			buf = append(buf, s[pos:]...)
-			break
-		}
-		start := pos + loc[0]
-		end := pos + loc[1]
-		buf = append(buf, s[pos:start]...)
-		buf = append(buf, repl...)
-		if end == start {
-			pos++
-		} else {
-			pos = end
-		}
-	}
-	return string(buf)
 }
 
 func (c *concurrentRegExpImpl) FindStringSubmatch(s string) []string {
@@ -339,6 +308,24 @@ func (c *concurrentRegExpImpl) FindStringSubmatch(s string) []string {
 	c.mu.Lock()
 	out := c.re.FindStringSubmatch(s)
 	c.mu.Unlock()
+	return out
+}
+
+func (c *concurrentRegExpImpl) FindAllSubmatch(b []byte, n int) [][][]byte {
+	locs := c.FindAllStringSubmatchIndex(string(b), n)
+	if len(locs) == 0 {
+		return nil
+	}
+	var out [][][]byte
+	for _, loc := range locs {
+		match := make([][]byte, len(loc)/2)
+		for i := 0; i < len(loc)/2; i++ {
+			if loc[2*i] >= 0 {
+				match[i] = b[loc[2*i]:loc[2*i+1]]
+			}
+		}
+		out = append(out, match)
+	}
 	return out
 }
 
@@ -387,6 +374,167 @@ func (c *concurrentRegExpImpl) findAllStringSubmatchCached(s string, n int) ([][
 		return nil, false
 	}
 	return out, false
+}
+
+func (c *concurrentRegExpImpl) FindSubmatchIndex(b []byte) []int {
+	return c.FindStringSubmatchIndex(string(b))
+}
+
+func (c *concurrentRegExpImpl) FindStringSubmatchIndex(s string) []int {
+	c.mu.RLock()
+	loc, cacheMiss := c.findStringIndexCached(s)
+	if cacheMiss {
+		c.mu.RUnlock()
+		c.mu.Lock()
+		out := c.re.FindStringSubmatchIndex(s)
+		c.mu.Unlock()
+		return out
+	}
+	if loc == nil {
+		c.mu.RUnlock()
+		return nil
+	}
+	if c.re.CaptureCount == 0 {
+		c.mu.RUnlock()
+		return []int{loc[0], loc[1]}
+	}
+	c.mu.RUnlock()
+	c.mu.Lock()
+	out := c.re.FindStringSubmatchIndex(s)
+	c.mu.Unlock()
+	return out
+}
+
+func (c *concurrentRegExpImpl) FindAllSubmatchIndex(b []byte, n int) [][]int {
+	return c.FindAllStringSubmatchIndex(string(b), n)
+}
+
+func (c *concurrentRegExpImpl) FindAllStringSubmatchIndex(s string, n int) [][]int {
+	c.mu.RLock()
+	if c.re.CaptureCount > 0 {
+		c.mu.RUnlock()
+		c.mu.Lock()
+		out := c.re.FindAllStringSubmatchIndex(s, n)
+		c.mu.Unlock()
+		return out
+	}
+	out, cacheMiss := c.findAllStringSubmatchIndexCached(s, n)
+	if !cacheMiss {
+		c.mu.RUnlock()
+		return out
+	}
+	c.mu.RUnlock()
+	c.mu.Lock()
+	res := c.re.FindAllStringSubmatchIndex(s, n)
+	c.mu.Unlock()
+	return res
+}
+
+func (c *concurrentRegExpImpl) findAllStringSubmatchIndexCached(s string, n int) ([][]int, bool) {
+	if c.re.CaptureCount > 0 {
+		return nil, true
+	}
+	var out [][]int
+	pos := 0
+	for pos <= len(s) && (n < 0 || len(out) < n) {
+		loc, cacheMiss := c.findStringIndexCached(s[pos:])
+		if cacheMiss {
+			return nil, true
+		}
+		if loc == nil {
+			break
+		}
+		start := pos + loc[0]
+		end := pos + loc[1]
+		out = append(out, []int{start, end})
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+	if len(out) == 0 {
+		return nil, false
+	}
+	return out, false
+}
+
+func (c *concurrentRegExpImpl) Split(s string, n int) []string {
+	if n == 0 {
+		return nil
+	}
+	if n == 1 {
+		return []string{s}
+	}
+	var result []string
+	pos := 0
+	splits := 0
+	maxSplits := n - 1
+	for pos <= len(s) {
+		loc := c.FindStringIndex(s[pos:])
+		if loc == nil {
+			result = append(result, s[pos:])
+			break
+		}
+		start := pos + loc[0]
+		end := pos + loc[1]
+		result = append(result, s[pos:start])
+		splits++
+		if n > 0 && splits >= maxSplits {
+			result = append(result, s[end:])
+			break
+		}
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+	return result
+}
+
+func (c *concurrentRegExpImpl) ReplaceAll(src, repl []byte) []byte {
+	var buf []byte
+	pos := 0
+	for pos <= len(src) {
+		loc := c.FindIndex(src[pos:])
+		if loc == nil {
+			buf = append(buf, src[pos:]...)
+			break
+		}
+		start := pos + loc[0]
+		end := pos + loc[1]
+		buf = append(buf, src[pos:start]...)
+		buf = append(buf, repl...)
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+	return buf
+}
+
+func (c *concurrentRegExpImpl) ReplaceAllString(s, repl string) string {
+	var buf []byte
+	pos := 0
+	for pos <= len(s) {
+		loc := c.FindStringIndex(s[pos:])
+		if loc == nil {
+			buf = append(buf, s[pos:]...)
+			break
+		}
+		start := pos + loc[0]
+		end := pos + loc[1]
+		buf = append(buf, s[pos:start]...)
+		buf = append(buf, repl...)
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+	return string(buf)
 }
 
 func (c *concurrentRegExpImpl) Clone() RegExp {
--- regexp.go
+++ regexp.go
@@ -16,21 +16,19 @@ type regexpImpl struct {
 	forwardTDFA  *lazyTDFA // built lazily when a submatch API is used
 }
 
-// Clone returns a new RegExp that shares minterms and root ASTs but has fresh
-// lazy DFA caches. Safe to use the original and clone in different goroutines.
-func (re *regexpImpl) Clone() RegExp {
-	return &regexpImpl{
-		minterms:     re.minterms,
-		forward:      newLazyDFA(re.forward.root, re.minterms),
-		unanchored:   newLazyDFA(re.unanchored.root, re.minterms),
-		reverse:      newLazyDFA(re.reverse.root, re.minterms),
-		prefix:       re.prefix,
-		CaptureCount: re.CaptureCount,
-		// forwardTDFA not copied; built on first submatch use
+// Match reports whether the byte slice b contains any match of the regular expression.
+func (re *regexpImpl) Match(b []byte) bool {
+	state := 0
+	for pos := 0; pos < len(b); {
+		r, size := utf8.DecodeRune(b[pos:])
+		mintermID := re.minterms.runeToClass(r)
+		state = re.forward.getNextState(state, mintermID)
+		pos += size
 	}
+	return re.forward.isAccepting(state)
 }
 
-// MatchString reports whether the string s exactly matches the regular expression.
+// MatchString reports whether the string s contains any match of the regular expression.
 // regexpImpl is not safe for concurrent use; use Clone() per goroutine or Concurrent() for a thread-safe wrapper.
 func (re *regexpImpl) MatchString(s string) bool {
 	state := 0
@@ -43,19 +41,14 @@ func (re *regexpImpl) MatchString(s string) bool {
 	return re.forward.isAccepting(state)
 }
 
-// Match reports whether the byte slice b exactly matches the regular expression.
-func (re *regexpImpl) Match(b []byte) bool {
-	state := 0
-	for pos := 0; pos < len(b); {
-		r, size := utf8.DecodeRune(b[pos:])
-		mintermID := re.minterms.runeToClass(r)
-		state = re.forward.getNextState(state, mintermID)
-		pos += size
-	}
-	return re.forward.isAccepting(state)
+// FindIndex returns a two-element slice of integers defining the location of the leftmost match in b.
+// The match itself is at b[loc[0]:loc[1]]. A return value of nil indicates no match.
+func (re *regexpImpl) FindIndex(b []byte) []int {
+	return re.FindStringIndex(string(b))
 }
 
-// FindStringIndex returns the [start, end] of the leftmost-longest match in O(n) time.
+// FindStringIndex returns a two-element slice of integers defining the location of the leftmost match in s.
+// The match itself is at s[loc[0]:loc[1]]. A return value of nil indicates no match.
 func (re *regexpImpl) FindStringIndex(s string) []int {
 	if len(s) == 0 {
 		if re.forward.isAccepting(0) {
@@ -149,16 +142,7 @@ func (re *regexpImpl) FindStringIndex(s string) []int {
 	return []int{leftmostStart, longestEnd}
 }
 
-// FindString returns a string holding the text of the leftmost-longest match.
-func (re *regexpImpl) FindString(s string) string {
-	loc := re.FindStringIndex(s)
-	if loc == nil {
-		return ""
-	}
-	return s[loc[0]:loc[1]]
-}
-
-// Find returns a slice holding the text of the leftmost-longest match in b.
+// Find returns a slice holding the text of the leftmost match in b. A return value of nil indicates no match.
 func (re *regexpImpl) Find(b []byte) []byte {
 	loc := re.FindStringIndex(string(b))
 	if loc == nil {
@@ -167,13 +151,64 @@ func (re *regexpImpl) Find(b []byte) []byte {
 	return b[loc[0]:loc[1]]
 }
 
-// FindIndex returns the [start, end] of the leftmost-longest match in b.
-func (re *regexpImpl) FindIndex(b []byte) []int {
-	return re.FindStringIndex(string(b))
+// FindString returns a string holding the text of the leftmost match in s.
+func (re *regexpImpl) FindString(s string) string {
+	loc := re.FindStringIndex(s)
+	if loc == nil {
+		return ""
+	}
+	return s[loc[0]:loc[1]]
 }
 
-// FindAllStringIndex returns a slice of all successive matches of the expression.
-// If n >= 0, the function returns at most n matches. Each match is [start, end].
+// FindAll is the 'All' version of Find; it returns a slice of all successive matches of the expression in b.
+// A return value of nil indicates no match.
+func (re *regexpImpl) FindAll(b []byte, n int) [][]byte {
+	s := string(b)
+	locs := re.FindAllStringIndex(s, n)
+	if len(locs) == 0 {
+		return nil
+	}
+	out := make([][]byte, len(locs))
+	for i, loc := range locs {
+		out[i] = b[loc[0]:loc[1]]
+	}
+	return out
+}
+
+// FindAllString is the 'All' version of FindString; it returns a slice of all successive matches of the expression.
+// A return value of nil indicates no match.
+func (re *regexpImpl) FindAllString(s string, n int) []string {
+	var matches []string
+	pos := 0
+
+	for pos <= len(s) && (n < 0 || len(matches) < n) {
+		loc := re.FindStringIndex(s[pos:])
+		if loc == nil {
+			break
+		}
+
+		start := pos + loc[0]
+		end := pos + loc[1]
+		matches = append(matches, s[start:end])
+
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+
+	return matches
+}
+
+// FindAllIndex is the 'All' version of FindIndex; it returns a slice of all successive matches of the expression in b.
+// A return value of nil indicates no match.
+func (re *regexpImpl) FindAllIndex(b []byte, n int) [][]int {
+	return re.FindAllStringIndex(string(b), n)
+}
+
+// FindAllStringIndex is the 'All' version of FindStringIndex; it returns a slice of all successive matches
+// of the expression. A return value of nil indicates no match.
 func (re *regexpImpl) FindAllStringIndex(s string, n int) [][]int {
 	var matches [][]int
 	pos := 0
@@ -198,123 +233,31 @@ func (re *regexpImpl) FindAllStringIndex(s string, n int) [][]int {
 	return matches
 }
 
-// FindAll returns a slice of all successive matches of the expression in b.
-// If n >= 0, the function returns at most n matches.
-func (re *regexpImpl) FindAll(b []byte, n int) [][]byte {
-	s := string(b)
-	locs := re.FindAllStringIndex(s, n)
-	if len(locs) == 0 {
+func (re *regexpImpl) FindSubmatch(b []byte) [][]byte {
+	loc := re.FindStringIndex(string(b))
+	if loc == nil {
 		return nil
 	}
-	out := make([][]byte, len(locs))
-	for i, loc := range locs {
-		out[i] = b[loc[0]:loc[1]]
+	if re.CaptureCount == 0 {
+		return [][]byte{b[loc[0]:loc[1]]}
+	}
+	if re.forwardTDFA == nil {
+		re.forwardTDFA = newLazyTDFA(injectCaptureTags(re.forward.root), re.minterms, re.CaptureCount)
+	}
+	span := string(b[loc[0]:loc[1]])
+	capture := re.forwardTDFA.runTDFA(span)
+	if capture == nil {
+		return nil
+	}
+	out := make([][]byte, 1+re.CaptureCount)
+	out[0] = b[loc[0]:loc[1]]
+	for i := 1; i <= re.CaptureCount; i++ {
+		start, end := capture[2*i], capture[2*i+1]
+		if start >= 0 && end >= start {
+			out[i] = b[loc[0]+start : loc[0]+end]
+		}
 	}
 	return out
-}
-
-// FindAllIndex returns a slice of all successive matches of the expression in b.
-// If n >= 0, the function returns at most n matches.
-func (re *regexpImpl) FindAllIndex(b []byte, n int) [][]int {
-	return re.FindAllStringIndex(string(b), n)
-}
-
-// Split slices s into substrings separated by the expression and returns a slice of
-// the substrings between those expression matches. If n > 0, at most n substrings are
-// returned; the last substring will be the unsplit remainder. If n <= 0, there is no limit.
-func (re *regexpImpl) Split(s string, n int) []string {
-	if n == 0 {
-		return nil
-	}
-
-	if n == 1 {
-		return []string{s}
-	}
-
-	var result []string
-	pos := 0
-	splits := 0
-	maxSplits := n - 1
-
-	for pos <= len(s) {
-		loc := re.FindStringIndex(s[pos:])
-		if loc == nil {
-			result = append(result, s[pos:])
-			break
-		}
-
-		start := pos + loc[0]
-		end := pos + loc[1]
-
-		result = append(result, s[pos:start])
-		splits++
-		if n > 0 && splits >= maxSplits {
-			result = append(result, s[end:])
-			break
-		}
-
-		if end == start {
-			pos++
-		} else {
-			pos = end
-		}
-	}
-
-	return result
-}
-
-// ReplaceAllString returns a copy of s, replacing matches of the expression with repl.
-func (re *regexpImpl) ReplaceAllString(s, repl string) string {
-	var buf []byte
-	pos := 0
-
-	for pos <= len(s) {
-		loc := re.FindStringIndex(s[pos:])
-		if loc == nil {
-			buf = append(buf, s[pos:]...)
-			break
-		}
-
-		start := pos + loc[0]
-		end := pos + loc[1]
-
-		buf = append(buf, s[pos:start]...)
-		buf = append(buf, repl...)
-
-		if end == start {
-			pos++
-		} else {
-			pos = end
-		}
-	}
-
-	return string(buf)
-}
-
-// FindAllString returns a slice of all successive matches of the expression.
-// If n >= 0, the function returns at most n matches.
-func (re *regexpImpl) FindAllString(s string, n int) []string {
-	var matches []string
-	pos := 0
-
-	for pos <= len(s) && (n < 0 || len(matches) < n) {
-		loc := re.FindStringIndex(s[pos:])
-		if loc == nil {
-			break
-		}
-
-		start := pos + loc[0]
-		end := pos + loc[1]
-		matches = append(matches, s[start:end])
-
-		if end == start {
-			pos++
-		} else {
-			pos = end
-		}
-	}
-
-	return matches
 }
 
 // FindStringSubmatch returns a slice of strings holding the text of the leftmost match
@@ -346,6 +289,24 @@ func (re *regexpImpl) FindStringSubmatch(s string) []string {
 		} else {
 			out[i] = ""
 		}
+	}
+	return out
+}
+
+func (re *regexpImpl) FindAllSubmatch(b []byte, n int) [][][]byte {
+	locs := re.FindAllStringSubmatchIndex(string(b), n)
+	if len(locs) == 0 {
+		return nil
+	}
+	var out [][][]byte
+	for _, loc := range locs {
+		match := make([][]byte, len(loc)/2)
+		for i := 0; i < len(loc)/2; i++ {
+			if loc[2*i] >= 0 {
+				match[i] = b[loc[2*i]:loc[2*i+1]]
+			}
+		}
+		out = append(out, match)
 	}
 	return out
 }
@@ -396,4 +357,200 @@ func (re *regexpImpl) FindAllStringSubmatch(s string, n int) [][]string {
 		return nil
 	}
 	return out
+}
+
+func (re *regexpImpl) FindSubmatchIndex(b []byte) []int {
+	return re.FindStringSubmatchIndex(string(b))
+}
+
+func (re *regexpImpl) FindStringSubmatchIndex(s string) []int {
+	loc := re.FindStringIndex(s)
+	if loc == nil {
+		return nil
+	}
+	if re.CaptureCount == 0 {
+		return []int{loc[0], loc[1]}
+	}
+	if re.forwardTDFA == nil {
+		re.forwardTDFA = newLazyTDFA(injectCaptureTags(re.forward.root), re.minterms, re.CaptureCount)
+	}
+	span := s[loc[0]:loc[1]]
+	capture := re.forwardTDFA.runTDFA(span)
+	if capture == nil {
+		return nil
+	}
+	// Mutate the fresh capture slice in-place to offset it to the global string index
+	capture[0] = loc[0]
+	capture[1] = loc[1]
+	for i := 1; i <= re.CaptureCount; i++ {
+		if capture[2*i] >= 0 {
+			capture[2*i] += loc[0]
+			capture[2*i+1] += loc[0]
+		}
+	}
+	return capture
+}
+
+func (re *regexpImpl) FindAllSubmatchIndex(b []byte, n int) [][]int {
+	return re.FindAllStringSubmatchIndex(string(b), n)
+}
+
+func (re *regexpImpl) FindAllStringSubmatchIndex(s string, n int) [][]int {
+	var out [][]int
+	pos := 0
+	for pos <= len(s) && (n < 0 || len(out) < n) {
+		loc := re.FindStringIndex(s[pos:])
+		if loc == nil {
+			break
+		}
+		start := pos + loc[0]
+		end := pos + loc[1]
+
+		if re.CaptureCount == 0 {
+			out = append(out, []int{start, end})
+		} else {
+			if re.forwardTDFA == nil {
+				re.forwardTDFA = newLazyTDFA(injectCaptureTags(re.forward.root), re.minterms, re.CaptureCount)
+			}
+			span := s[start:end]
+			capture := re.forwardTDFA.runTDFA(span)
+			if capture == nil {
+				out = append(out, []int{start, end})
+			} else {
+				capture[0] = start
+				capture[1] = end
+				for i := 1; i <= re.CaptureCount; i++ {
+					if capture[2*i] >= 0 {
+						capture[2*i] += start
+						capture[2*i+1] += start
+					}
+				}
+				out = append(out, capture)
+			}
+		}
+
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+	if len(out) == 0 {
+		return nil
+	}
+	return out
+}
+
+// Split slices s into substrings separated by the expression and returns a slice of
+// the substrings between those expression matches. If n > 0, at most n substrings are
+// returned; the last substring will be the unsplit remainder. If n <= 0, there is no limit.
+func (re *regexpImpl) Split(s string, n int) []string {
+	if n == 0 {
+		return nil
+	}
+
+	if n == 1 {
+		return []string{s}
+	}
+
+	var result []string
+	pos := 0
+	splits := 0
+	maxSplits := n - 1
+
+	for pos <= len(s) {
+		loc := re.FindStringIndex(s[pos:])
+		if loc == nil {
+			result = append(result, s[pos:])
+			break
+		}
+
+		start := pos + loc[0]
+		end := pos + loc[1]
+
+		result = append(result, s[pos:start])
+		splits++
+		if n > 0 && splits >= maxSplits {
+			result = append(result, s[end:])
+			break
+		}
+
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+
+	return result
+}
+
+// ReplaceAll returns a copy of src, replacing matches of the expression with repl.
+func (re *regexpImpl) ReplaceAll(src, repl []byte) []byte {
+	var buf []byte
+	pos := 0
+
+	for pos <= len(src) {
+		loc := re.FindIndex(src[pos:])
+		if loc == nil {
+			buf = append(buf, src[pos:]...)
+			break
+		}
+
+		start := pos + loc[0]
+		end := pos + loc[1]
+
+		buf = append(buf, src[pos:start]...)
+		buf = append(buf, repl...)
+
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+
+	return buf
+}
+
+// ReplaceAllString returns a copy of s, replacing matches of the expression with repl.
+func (re *regexpImpl) ReplaceAllString(s, repl string) string {
+	var buf []byte
+	pos := 0
+
+	for pos <= len(s) {
+		loc := re.FindStringIndex(s[pos:])
+		if loc == nil {
+			buf = append(buf, s[pos:]...)
+			break
+		}
+
+		start := pos + loc[0]
+		end := pos + loc[1]
+
+		buf = append(buf, s[pos:start]...)
+		buf = append(buf, repl...)
+
+		if end == start {
+			pos++
+		} else {
+			pos = end
+		}
+	}
+
+	return string(buf)
+}
+
+// Clone returns a new RegExp that shares minterms and root ASTs but has fresh
+// lazy DFA caches. Safe to use the original and clone in different goroutines.
+func (re *regexpImpl) Clone() RegExp {
+	return &regexpImpl{
+		minterms:     re.minterms,
+		forward:      newLazyDFA(re.forward.root, re.minterms),
+		unanchored:   newLazyDFA(re.unanchored.root, re.minterms),
+		reverse:      newLazyDFA(re.reverse.root, re.minterms),
+		prefix:       re.prefix,
+		CaptureCount: re.CaptureCount,
+		// forwardTDFA not copied; built on first submatch use
+	}
 }
